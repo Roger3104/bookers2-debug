398392e78f1310103d2f76141faf5ada80cce042
EC2 Default User <119906602+Roger3104@users.noreply.github.com>
2023-02-06 16:34:09 +0000

    [Finish] 7a

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,11 +4,11 @@ class Book < ApplicationRecord
  has_many :book_comments, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited?(user)
    favorites.where(user_id: user.id).exists?
   end
-  
+
   # 検索方法分岐
   def self.search_for(content, method)
     if method == 'perfect'
@@ -21,7 +21,7 @@ class Book < ApplicationRecord
       Book.where('title LIKE ?', '%'+content+'%')
     end
   end
-  
+
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } #今週
